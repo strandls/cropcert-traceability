2971bb3dd5a71f9b4fcabef1c285da623631faa4
vilay <kandi.vilay09@gmail.com>
2019-05-31 15:13:34 +0530

    feat: Wet process updation

--- src/cropcert/traceability/wetbatch/WetBatchEndPoint.java
+++ src/cropcert/traceability/wetbatch/WetBatchEndPoint.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -78,5 +79,41 @@ public class WetBatchEndPoint {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	@PUT
+	@Path("startTime")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public WetBatch updateStartTime(String jsonString) throws JSONException {
+		WetBatch wetBatch = wetbatchService.updateStartTime(jsonString);
+		return wetBatch;
+	}
+	
+	@PUT
+	@Path("fermentationEndTime")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public WetBatch updateFermentationEndTime(String jsonString) throws JSONException {
+		WetBatch wetBatch = wetbatchService.updateFermentationEndTime(jsonString);
+		return wetBatch;
+	}
+	
+	@PUT
+	@Path("dryingEndTime")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public WetBatch updateDryingEndTime(String jsonString) throws JSONException {
+		WetBatch wetBatch = wetbatchService.updateDryingEndTime(jsonString);
+		return wetBatch;
+	}
+	
+	@PUT
+	@Path("perchmentQuantity")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public WetBatch updatePerchmentQuantity(String jsonString) throws JSONException {
+		WetBatch wetBatch = wetbatchService.updatePerchmentQuantity(jsonString);
+		return wetBatch;
 	}
 }
--- src/cropcert/traceability/wetbatch/WetBatchService.java
+++ src/cropcert/traceability/wetbatch/WetBatchService.java
@@ -1,8 +1,10 @@
 package cropcert.traceability.wetbatch;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -25,5 +27,45 @@ public class WetBatchService extends AbstractService<WetBatch> {
 			throws JsonParseException, JsonMappingException, IOException, JSONException {
 		WetBatch batch = objectMappper.readValue(jsonString, WetBatch.class);
 		return save(batch);
+	}
+	
+	public WetBatch updateStartTime(String jsoString) throws JSONException {
+		JSONObject jsonObject = new JSONObject(jsoString);
+		Long id = jsonObject.getLong("id");
+		Timestamp startTime = (Timestamp) jsonObject.get("startTime");
+		
+		WetBatch wetBatch = findById(id);
+		wetBatch.setStartTime(startTime);
+		return update(wetBatch);
+	}
+	
+	public WetBatch updateFermentationEndTime(String jsoString) throws JSONException {
+		JSONObject jsonObject = new JSONObject(jsoString);
+		Long id = jsonObject.getLong("id");
+		Timestamp fermentationEndTime = (Timestamp) jsonObject.get("fermentationEndTime");
+		
+		WetBatch wetBatch = findById(id);
+		wetBatch.setFermentationEndTime(fermentationEndTime);
+		return update(wetBatch);
+	}
+	
+	public WetBatch updateDryingEndTime(String jsoString) throws JSONException {
+		JSONObject jsonObject = new JSONObject(jsoString);
+		Long id = jsonObject.getLong("id");
+		Timestamp dryingEndTime = (Timestamp) jsonObject.get("dryingEndTime");
+		
+		WetBatch wetBatch = findById(id);
+		wetBatch.setDryingEndTime(dryingEndTime);
+		return update(wetBatch);
+	}
+	
+	public WetBatch updatePerchmentQuantity(String jsoString) throws JSONException {
+		JSONObject jsonObject = new JSONObject(jsoString);
+		Long id = jsonObject.getLong("id");
+		float perchmentQuantity = (float) jsonObject.get("perchmentQuantity");
+		
+		WetBatch wetBatch = findById(id);
+		wetBatch.setPerchmentQuantity(perchmentQuantity);
+		return update(wetBatch);
 	}
 }
