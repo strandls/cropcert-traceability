2439a57916e86a065d270c78613eabeb9d779cdc
vilay <kandi.vilay09@gmail.com>
2019-07-01 18:53:34 +0530

    fix : code revert for save

--- src/cropcert/traceability/wetbatch/WetBatchEndPoint.java
+++ src/cropcert/traceability/wetbatch/WetBatchEndPoint.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,8 +16,11 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.json.JSONException;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.google.inject.Inject;
 
 @Path("wetbatch")
@@ -59,6 +63,32 @@ public class WetBatchEndPoint {
 			@DefaultValue("-1") @QueryParam("limit") Integer limit,
 			@DefaultValue("-1") @QueryParam("offset") Integer offset) {
 		return wetbatchService.getByPropertyfromArray("ccCode", ccCodes, limit, offset);
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response save(String  jsonString) {
+		try {
+			WetBatch wetBatch = wetbatchService.save(jsonString);
+			return Response.status(Status.CREATED).entity(wetBatch).build();
+		} catch(ConstraintViolationException e) {
+			return Response.status(Status.CONFLICT).tag("Dublicate key").build();
+		}
+		catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 	
 	@PUT
