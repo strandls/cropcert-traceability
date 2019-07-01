d9ec75537c29f46a12d5829ca2d0468721e7c9a9
Harsh Zalavadiya <harsh@strandls.com>
2019-07-01 18:31:54 +0530

    fix: string to long array

--- src/cropcert/traceability/batch/BatchService.java
+++ src/cropcert/traceability/batch/BatchService.java
@@ -22,47 +22,50 @@ import cropcert.traceability.util.UserUtil;
 
 public class BatchService extends AbstractService<Batch> {
 
-	@Inject
-	private ObjectMapper objectMappper;
-	
-	@Inject
-	private ActivityService activityService;
+    @Inject
+    private ObjectMapper objectMappper;
 
-	@Inject
-	public BatchService(BatchDao dao) {
-		super(dao);
-	}
+    @Inject
+    private ActivityService activityService;
 
-	public Batch save(String jsonString, HttpServletRequest request)
-			throws JsonParseException, JsonMappingException, IOException, JSONException {
-		Batch batch = objectMappper.readValue(jsonString, Batch.class);
-		
-		// update the transfer time stamp
-		Timestamp transferTimestamp = batch.getCreatedOn();
-		if (transferTimestamp == null) {
-			transferTimestamp = new Timestamp(new Date().getTime());
-			batch.setCreatedOn(transferTimestamp);
-		}
-		batch = save(batch);
-		
-		/**
-		 * save the activity here.
-		 */
-		String userId = UserUtil.getUserDetails(request);
-		Timestamp timestamp = transferTimestamp = new Timestamp(new Date().getTime());
-		Activity activity = new Activity(Constants.BATCH, batch.getBatchId(), userId, 
-				timestamp, "batch_creation", batch.getBatchName());
-		activity = activityService.save(activity);
-		
-		return batch;
-	}
-	
-	/*
+    @Inject
+    public BatchService(BatchDao dao) {
+        super(dao);
+    }
+
+    public Batch save(String jsonString, HttpServletRequest request)
+            throws JsonParseException, JsonMappingException, IOException, JSONException {
+        Batch batch = objectMappper.readValue(jsonString, Batch.class);
+
+        // update the transfer time stamp
+        Timestamp transferTimestamp = batch.getCreatedOn();
+        if (transferTimestamp == null) {
+            transferTimestamp = new Timestamp(new Date().getTime());
+            batch.setCreatedOn(transferTimestamp);
+        }
+        batch = save(batch);
+
+        /**
+         * save the activity here.
+         */
+        String userId = UserUtil.getUserDetails(request);
+        Timestamp timestamp = transferTimestamp = new Timestamp(new Date().getTime());
+        Activity activity = new Activity(Constants.BATCH, batch.getBatchId(), userId,
+                timestamp, "batch_creation", batch.getBatchName());
+        activity = activityService.save(activity);
+
+        return batch;
+    }
+
+    /*
 	 * This object list is the comma separated value.
-	 */
-	public List<Batch> getByPropertyfromArray(String property, String objectList, int limit, int offset){
-		Object [] values = objectList.split(",");
-		((BatchDao) dao).getByPropertyfromArray(property, values, limit, offset);
-		return null;
-	}
+     */
+    public List<Batch> getByPropertyfromArray(String property, String objectList, int limit, int offset) {
+        Object[] values = objectList.split(",");
+        Long[] longValues = new Long[values.length];
+        for (int i = 0; i < values.length; i++) {
+            longValues[i] = Long.parseLong(values[i].toString());
+        }
+        return ((BatchDao) dao).getByPropertyfromArray(property, longValues, limit, offset);
+    }
 }
