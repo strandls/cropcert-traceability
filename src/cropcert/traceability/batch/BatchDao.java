13a74fa59f9d9511d2971c6276a26601c6c5d906
vilay <kandi.vilay09@gmail.com>
2019-07-02 14:18:59 +0530

    feat : new query to select batches filtered on status

--- src/cropcert/traceability/batch/BatchDao.java
+++ src/cropcert/traceability/batch/BatchDao.java
@@ -1,7 +1,13 @@
 package cropcert.traceability.batch;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 
 import com.google.inject.Inject;
 
@@ -26,5 +32,32 @@ public class BatchDao extends AbstractDao<Batch, Long>{
 			session.close();
 		}
 		return entity;
+	}
+	
+	public List<Batch> getByPropertyfromArray(String property, Object[] values, int limit, int offset) {
+		
+		/*
+		 * queary to get all batches from collection center ids specified in the values and 
+		 * the status of the batch is Not lot done.
+		 */
+		String queryStr = "" +
+			    "from "+daoType.getSimpleName()+" t " +
+			    "where t."+property+" in (:values) and isLotDone = false" +
+			    " order by id";
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery(queryStr);
+		query.setParameterList("values", values);
+
+		List<Batch> resultList = new ArrayList<Batch>();
+		try {
+			if(limit>0 && offset >= 0)
+				query = query.setFirstResult(offset).setMaxResults(limit);
+			resultList = query.getResultList();
+			
+		} catch (NoResultException e) {
+			throw e;
+		}
+		session.close();
+		return resultList;
 	}
 }
