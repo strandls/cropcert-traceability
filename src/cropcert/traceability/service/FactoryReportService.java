073de73677b4010e7ba37843d68a8e1265d87727
vilay <kandi.vilay09@gmail.com>
2019-12-20 18:03:12 +0530

    fix : factory status updated

--- src/cropcert/traceability/service/FactoryReportService.java
+++ src/cropcert/traceability/service/FactoryReportService.java
@@ -9,12 +9,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 
+import cropcert.traceability.ActionStatus;
 import cropcert.traceability.Constants;
 import cropcert.traceability.dao.FactoryReportDao;
 import cropcert.traceability.model.Activity;
@@ -51,6 +53,12 @@ public class FactoryReportService extends AbstractService<FactoryReport> {
 		
 		lot.setFactoryReportId(factoryReport.getId());
 		lotService.update(lot);
+		
+		JSONObject jsonObject = new JSONObject(jsonString);
+		if(jsonObject.has(Constants.FINALIZE_FACTORY_STATUS) && jsonObject.getBoolean(Constants.FINALIZE_FACTORY_STATUS))
+			lot.setFactoryStatus(ActionStatus.DONE);
+		else
+			lot.setFactoryStatus(ActionStatus.EDIT);
 		
 		 /**
          * save the activity here.
